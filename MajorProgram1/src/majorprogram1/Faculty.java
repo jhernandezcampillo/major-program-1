0b7ea232c9064c21f509589e17392bd05287cc51
Judith Hernandez <jhernandez-campillo@aggies.ncat.edu>
2019-02-27 00:50:34 -0500

    Faculty start

---
+++ MajorProgram1/src/majorprogram1/Faculty.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package majorprogram1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Judith
+ */
+public class Faculty {
+   private String firstName;
+   private String lastName;
+   private String officeLocation;
+   private ArrayList<Course> courses;
+   private ArrayList<TimeBlock> officeHours;
+   private ArrayList<Appointment> appointments;
+   
+   public Faculty(){
+       firstName = "";
+       lastName = "";
+       officeLocation = "";
+       courses = new ArrayList<>();
+       officeHours = new ArrayList<>();
+       appointments = new ArrayList<>();
+   }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the officeLocation
+     */
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    /**
+     * @param officeLocation the officeLocation to set
+     */
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation;
+    }
+   
+    public int getNumCourses(){
+        return courses.size();
+    }
+    public Course getCourse(int index){
+        return courses.get(index);
+    }
+    public void setCourse(int index, Course item){
+        courses.set(index, item);
+    }
+    public void addCourse(Course time){
+        courses.add(time);
+    }
+    public Course removeCourse(int index){
+        return courses.remove(index);
+    }
+    
+    public int getNumOfficeHours(){
+        return officeHours.size();
+    }
+    public TimeBlock getTimeBlock(int index){
+        return officeHours.get(index);
+    }
+    public void setTimeBlock(int index, TimeBlock item){
+        officeHours.set(index, item);
+    }
+    public void addTimeBlock(TimeBlock time){
+        officeHours.add(time);
+    }
+    public TimeBlock removeTimeBlock(int index){
+        return officeHours.remove(index);
+    }
+    
+    public int getNumAppointments(){
+        return appointments.size();
+    }
+    public Appointment getAppointment(int index){
+        return appointments.get(index);
+    }
+    public void setAppointment(int index, Appointment item){
+        appointments.set(index, item);
+    }
+    public void addAppointment(Appointment time){
+        appointments.add(time);
+    }
+    public Appointment removeAppointment(int index){
+        return appointments.remove(index);
+    }
+}
